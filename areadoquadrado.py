58371731c2e239d04e6d12a96419afdadd1c874b
Lucas Marques <lucasmarq1410@gmail.com>
2024-05-22 09:50:11 -0300

    pronto

--- areadoquadrado.py
+++ areadoquadrado.py
@@ -2,4 +2,4 @@ area = float(input("digite a área do quadrado: "))
 areatotal = area * area
 print (f"a área do quadrado é: {areatotal}")
 areadobro = areatotal*2
-print(f"o dobro dessa área é: {areadobro}")
+print(f"o dobro dessa área é: {areadobro}ok")
