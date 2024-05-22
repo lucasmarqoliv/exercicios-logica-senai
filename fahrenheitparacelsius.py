3fb0355c082102e76f9d2a6a665cdfecea14ae49
Lucas Marques <lucasmarq1410@gmail.com>
2024-05-22 10:58:16 -0300

    pronto

---
+++ fahrenheitparacelsius.py
@@ -0,0 +1,3 @@
+tempfah = float(input("digite a temperatura em graus fahrenheit: "))
+tempcels = 5*(tempfah-32)/9
+print(f"a temperatura digitada em Fahrenheit: {tempfah:.2f} ºF convertida em Celsius fica: {tempcels:.2f} ºC")
