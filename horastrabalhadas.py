dc02be71ff8dd2dbc946acaf7cf19a7aa14788b9
Lucas Marques <lucasmarq1410@gmail.com>
2024-05-22 10:41:34 -0300

    up

--- horastrabalhadas.py
+++ horastrabalhadas.py
@@ -1,4 +1,4 @@
 salario = float(input("quanto voce ganha por hora? "))
 horas = float(input("quantas horas voce trabalhou no mês? "))
 salarioDoMes = salario*horas
-print(f"o total do seu salario no mês é: {salarioDoMes:.2f} R$")
+print(f"ganhando R$ {salario:.2f} por hora, e tendo trabalhado {horas} no mes, o total do seu salario no mês é: R$ {salarioDoMes:.2f}")
