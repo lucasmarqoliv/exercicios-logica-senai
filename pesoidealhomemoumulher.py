9536cec0de1400284dcb1ee71cf5df24fbb4b1dd
Lucas Marques <lucasmarq1410@gmail.com>
2024-05-23 11:07:23 -0300

    pronto

---
+++ pesoidealhomemoumulher.py
@@ -0,0 +1,9 @@
+h = float(input("digite sua altura: "))
+sexo = str(input("sexo: "))
+if (sexo == "masculino"): 
+  peso_ideal_homem = (72.7*h)-58
+  print (f"seu peso ideal é: {peso_ideal_homem:.2f}")
+elif (sexo == "feminino"):
+  peso_ideal_mulher = (62.1*h)-44.7
+  print (f"seu peso ideal é: {peso_ideal_mulher:.2f}")
+
