968ecae0ed1cd31a92073e21e08a8d1abaf17c51
Lucas Marques <lucasmarq1410@gmail.com>
2024-05-24 10:24:06 -0300

    pronto

---
+++ numeromaior.py
@@ -0,0 +1,6 @@
+num1 = float(input("digite um numero: "))
+num2 = float(input("digite outro numero: "))
+if num1 > num2:
+    print (f"{num1:.2f} é maior que {num2:.2f}")
+elif num2 > num1:
+    print (f"{num2:.2f} é maior que {num1:.2f}")
