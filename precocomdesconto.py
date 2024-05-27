9a413921ae79452ddb8d131957ee021476bbebdc
karllenh ribeiro <lucasmarq1410@gmail.com>
2024-05-27 09:26:12 -0300

    pronto

---
+++ precocomdesconto.py
@@ -0,0 +1,4 @@
+produto = float(input('digite o preço do produto: '))
+desconto = (produto*5)/100
+produto_com_desconto = (produto+desconto)
+print ('o preço do produto com desconto de 5% fica: {:.2f}' .format(produto_com_desconto))   
