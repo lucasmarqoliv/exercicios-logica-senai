68cccb54fe7228cb3b91669c477a762e77ca0161
karllenh ribeiro <lucasmarq1410@gmail.com>
2024-05-27 09:34:25 -0300

    pronto

---
+++ salariocomaumento.py
@@ -0,0 +1,4 @@
+salario = float(input('digite seu salario: '))
+aumento = (salario * 15)/100
+salario_com_aumento = salario + aumento
+print ('seu salario de R$ {:.4f} com aumento de 15% fica R$ {:.4f}' .format (salario, salario_com_aumento, salario_com_aumento))
